1d7fbffcdf149555a45e1926feefe7b25c063815
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-11-20 13:19:04 -0400

    update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,32 @@
 <?php
 
 declare(strict_types=1);
-
-use Hyperf\Context\ApplicationContext;
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\Contract\ApplicationInterface;
 use Hyperf\Di\ClassLoader;
-use Hyperf\Di\Container;
-use Hyperf\Di\Definition\DefinitionSourceFactory;
+use Hyperf\Engine\DefaultOption;
 
-require_once __DIR__ .'/../vendor/autoload.php';
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
+
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
-(function () {
-    ClassLoader::init();
+require BASE_PATH . '/vendor/autoload.php';
 
-    ApplicationContext::setContainer(
-        new Container((new DefinitionSourceFactory)())
-    );
+! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', DefaultOption::hookFlags());
 
-    // $container->get(Hyperf\Contract\ApplicationInterface::class);
-})();
+ClassLoader::init();
+
+$container = require BASE_PATH . '/config/container.php';
+
+$container->get(ApplicationInterface::class);
